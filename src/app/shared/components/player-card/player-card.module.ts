d7cc9913c68af9f84a493556703df8baf7402fcb
Alejandro Rodriguez Romero <alex98rodriguezromero@gmail.com>
2021-05-30 13:03:31 -0500

    refactor: create component blank for player card

---
+++ src/app/shared/components/player-card/player-card.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { PlayerCardComponent } from './player-card.component';
+
+@NgModule({
+  exports: [PlayerCardComponent],
+  declarations: [PlayerCardComponent]
+})
+export class PlayerCardModule { }
