800e8a3896c1d7e811e915d1fc6ad05d7ca3dd1c
Alejandro Rodriguez Romero <alex98rodriguezromero@gmail.com>
2021-05-29 21:26:46 -0500

    style: modify name variable in user service

--- src/app/shared/services/user/user.service.ts
+++ src/app/shared/services/user/user.service.ts
@@ -5,26 +5,26 @@ import { BehaviorSubject, Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class UserService {
-  private username: BehaviorSubject<string | null> = new BehaviorSubject<string | null>(null);
+  private _name: BehaviorSubject<string | null> = new BehaviorSubject<string | null>(null);
 
-  get name(): Observable<string | null> {
-    return this.username.asObservable();
+  get name$(): Observable<string | null> {
+    return this._name.asObservable();
   }
 
   constructor() {
     const storage = sessionStorage.getItem('name');
     if (storage) {
-      this.username.next(storage);
+      this._name.next(storage);
     }
   }
 
   setName(name: string): void {
-    this.username.next(name);
+    this._name.next(name);
     sessionStorage.setItem('name', name);
   }
 
   removeName(): void {
     sessionStorage.removeItem('name');
-    this.username.next(null);
+    this._name.next(null);
   }
 }
